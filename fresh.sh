22112ff1da7fef588ebfc55a1cbe020104a0f41c
Sergey Karchmit <skarchmit@gmail.com>
2020-08-02 17:05:01 -0700

    Copy .zshrc to $HOME
    Symlink is broken when starting up terminal

--- fresh.sh
+++ fresh.sh
@@ -26,8 +26,9 @@ brew cleanup
 # ./clone.sh
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+# rm -rf $HOME/.zshrc
+# ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+cp $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
