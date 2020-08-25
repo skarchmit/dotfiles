81469e79ef9783bad53af65c7d300830458a9fd3
Sergey Karchmit <skarchmit@gmail.com>
2020-08-25 14:31:24 -0700

    add aliases to remove git branches

--- aliases.zsh
+++ aliases.zsh
@@ -1,3 +1,10 @@
 alias cpy='conda activate base && python'
 alias ls='ls -lashG'
 alias code='code-insiders'
+alias cenv='conda env update -f environment.yml'
+
+# delete all local branches except master
+alias gbra="git checkout master && git branch | grep -v "master" | xargs git branch -D"
+
+# delete local branches that dont exist in remote
+alias gbr=git remote prune origin  
